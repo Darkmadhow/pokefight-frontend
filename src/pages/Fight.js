c47501f515e535ac668b988871ec2cfe2588c48a
Friis Kraas <friis.kraas@gmx.de>
2023-05-24 17:52:44 +0200

    implemented pokemon loading in Fight.js

--- src/pages/Fight.js
+++ src/pages/Fight.js
@@ -1,13 +1,25 @@
 import { useParams } from "react-router-dom";
 import { getPokemonById } from "../utils/PokeAPI";
+import { useEffect, useState } from "react";
 
 export default function Fight() {
+  const [myPokemon, setMyPokemon] = useState({});
+  const [enemyPokemon, setEnemyPokemon] = useState({});
   const { id } = useParams();
-  const myPokemon = getPokemonById(id);
   const rndID = Math.floor(
     Math.random() * ProcessingInstruction.env.POKEMON_MAX + 1
   );
-  const enemyPokemon = getPokemonById(rndID);
+
+  async function loadPokemon() {
+    let myP = await getPokemonById(id);
+    let enP = await getPokemonById(rndID);
+    setMyPokemon(myP);
+    setEnemyPokemon(enP);
+  }
+
+  useEffect(() => {
+    loadPokemon();
+  }, []);
 
   return <div className="fightPage"></div>;
 }
