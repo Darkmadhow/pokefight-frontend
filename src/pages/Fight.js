b7852d4b55955be46a0760bbe6f6ba4391e7728f
Friis Kraas <friis.kraas@gmx.de>
2023-05-25 16:42:50 +0200

    implemented imageloading and fixed pokemonloading

--- src/pages/Fight.js
+++ src/pages/Fight.js
@@ -1,25 +1,60 @@
 import { useParams } from "react-router-dom";
 import { getPokemonById } from "../utils/PokeAPI";
 import { useEffect, useState } from "react";
+import getPokemonImages from "../utils/ImageAPI";
+import "./Fight.css";
 
 export default function Fight() {
-  const [myPokemon, setMyPokemon] = useState({});
-  const [enemyPokemon, setEnemyPokemon] = useState({});
+  console.log(
+    "Maximum amount of Pokemon is ",
+    process.env.REACT_APP_POKEMON_MAX
+  );
+  const [{ myPokemon, myPokemonSprites }, setMyPokemon] = useState({
+    undefined,
+    undefined,
+  });
+  const [{ enemyPokemon, enemyPokemonSprites }, setEnemyPokemon] = useState({
+    undefined,
+    undefined,
+  });
   const { id } = useParams();
   const rndID = Math.floor(
-    Math.random() * ProcessingInstruction.env.POKEMON_MAX + 1
+    Math.random() * process.env.REACT_APP_POKEMON_MAX + 1
   );
 
   async function loadPokemon() {
-    let myP = await getPokemonById(id);
-    let enP = await getPokemonById(rndID);
-    setMyPokemon(myP);
-    setEnemyPokemon(enP);
+    const myPokemon = await getPokemonById(id);
+    const enemyPokemon = await getPokemonById(rndID);
+    const myPokemonSprites = await getPokemonImages(myPokemon.name.english);
+    const enemyPokemonSprites = await getPokemonImages(
+      enemyPokemon.name.english
+    );
+    setMyPokemon({ myPokemon, myPokemonSprites });
+    setEnemyPokemon({ enemyPokemon, enemyPokemonSprites });
   }
 
   useEffect(() => {
     loadPokemon();
   }, []);
 
-  return <div className="fightPage"></div>;
+  return (
+    <div className="fightPage">
+      <div className="battlefield">
+        <div className="enemyPokeStats">{enemyPokemon?.base.HP}</div>
+        <div className="enemyPokeSprite">
+          <img
+            src={enemyPokemonSprites?.front_default}
+            alt={enemyPokemon?.name.english}
+          />
+        </div>
+        <div className="myPokeSprite">
+          <img
+            src={myPokemonSprites?.back_default}
+            alt={myPokemon?.name.english}
+          />
+        </div>
+        <div className="myPokeStats">{myPokemon?.base.HP}</div>
+      </div>
+    </div>
+  );
 }
